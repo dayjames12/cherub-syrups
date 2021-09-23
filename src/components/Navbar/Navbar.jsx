94a32361d7925dbecf81a9c0b0fb63ab3a981bb2
dayjames12 <dayjames12@gmail.com>
2021-09-23 00:58:20 -0700

    Removed files not needed from create-react-app. Starting creating setup largly in regards to ecommerce functionality and small bit of ui for website. Currently moving forward with ecommerce.js for the emcommerce backend but may switch directions and go with shopify.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Badge,
+    MenuItem,
+    Menu,
+    Typography,
+} from '@material-ui/core'
+import { ShoppingCart } from '@material-ui/icons'
+
+import logo from '../../assets/cherub.png'
+import useStyles from './styles'
+
+const Navbar = () => {
+    const classes = useStyles()
+    return (
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    className={classes.title}
+                    color="inherit"
+                >
+                    <img
+                        src={logo}
+                        alt="Cherub Syrups"
+                        height="50px"
+                        className={classes.image}
+                    />
+                    Cherub Syrups
+                </Typography>
+                <div className={classes.grow} />
+                <div className={classes.IconButton}>
+                    <IconButton aria-label="Show cart items" color="inherit">
+                        <Badge badgeContent={2} color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                </div>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar
